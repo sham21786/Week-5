14c38b6489e18f1d3650f3ab8c95cf362e79d8c6
Shazmil Nassar <sham21786@yahoo.com>
2015-05-08 15:09:43 +0100

    week 5

---
+++ Assets/Entry.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Entry : MonoBehaviour {
+
+	Text textComponent;
+	int points;
+	int rank;
+
+	// Use this for initialization
+	void Start () {
+	
+		textComponent = GetComponent<Text> ();
+		points = Random.Range (0, 100);
+	}
+
+	void Update (){
+
+		textComponent.text = string.Format (" - {0}. {1} | {2}", rank, name, points);
+	}
+
+	public int ComparePoints(Entry other) {
+		
+		return other.points.CompareTo(points);
+	}
+
+	public int CompareName(Entry other) {
+		
+		return name.CompareTo(other.name);
+	}
+
+	void SetRank(int newRank) {
+		
+		rank = newRank;
+		Transform parent = transform.parent;
+		transform.SetParent (null);
+		transform.SetParent (parent);
+	}
+
+	void SetAlphaPosition(int position) {
+		
+		Transform parent = transform.parent;
+		transform.SetParent (null);
+		transform.SetParent (parent);
+	}
+}
---
+++ Assets/Leaderboard.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Leaderboard : MonoBehaviour {
+
+	public void SortByPoints() {
+
+		List<Entry> list = new List<Entry> (GetComponentsInChildren<Entry> ());
+
+		list.Sort ((anything, anythingElse) => { return anything.ComparePoints(anythingElse); });
+
+		for (int rank = 0; rank < list.Count; ++rank) {
+
+			list[rank].SendMessage ("SetRank", rank + 1);
+		}
+	}
+
+	public void SortByName() {
+		
+		List<Entry> list = new List<Entry> (GetComponentsInChildren<Entry> ());
+		
+		list.Sort ((anything, anythingElse) => { return anything.CompareName(anythingElse); });
+		
+		for (int alphaPosition = 0; alphaPosition < list.Count; ++alphaPosition) {
+			
+			list[alphaPosition].SendMessage ("SetAlphaPosition", alphaPosition + 1);
+		}
+	}
+
+}
